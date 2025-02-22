4821e6cb31a021c5c85496d7eb2678e48de392d3
Darcy <drc@cmail.com>
2025-02-22 18:22:13 +0200

    sample data fetching of client

--- frontend/app/hooks/useSocket.ts
+++ frontend/app/hooks/useSocket.ts
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import io, { Socket } from "socket.io-client";
+import { nextEvents } from "../../nextEvents";
+import { EventData } from "../../../events";
 
 export const useSocket = () => {
 	const [socket, setSocket] = useState<Socket | null>(null);
 	const [isConnected, setIsConnected] = useState(false);
 	const [messages, setMessages] = useState<string[]>([]);
+
+	const [data, setData] = useState<EventData[]>([]);
 
 	useEffect(() => {
 		const socketIo = io();
@@ -21,6 +25,11 @@ export const useSocket = () => {
 			setMessages((prevMessages) => [...prevMessages, msg]);
 		});
 
+		socketIo.on(nextEvents.DATA_SEND, (data: EventData[]) => {
+			console.log(data);
+			setData(data);
+		});
+
 		setSocket(socketIo);
 
 		return () => {
@@ -34,5 +43,11 @@ export const useSocket = () => {
 		}
 	};
 
-	return { isConnected, messages, sendMessage };
+	const openHost = (hostId: string) => {
+		if (socket) {
+			socket.emit(nextEvents.HOST_OPEN, { hostId });
+		}
+	};
+
+	return { isConnected, messages, sendMessage, openHost, data };
 };
